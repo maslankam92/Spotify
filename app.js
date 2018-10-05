45ac653422a7fb25a7af35f6dd14bef2dee53545
Marcin Maslanka <maslankam92@gmail.com>
2018-10-05 14:22:40 +0200

    Configuration of spotify access token

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
-document.addEventListener('DOMContentLoaded', function () {
+const SEARCH_URL = 'https://api.spotify.com/v1/search?q=';
+const CLIENT_ID = '739bb8c6d81047a59b9e77ccb475d91c';
+const URI = 'http://localhost:63340/Spotify/index.html';
 
-  // const SEARCH_URL = 'https://api.spotify.com/v1/search';
-  // const CLIENT_ID = '739bb8c6d81047a59b9e77ccb475d91c';
-  // const URI = 'http://localhost:63340/Spotify/index.html';
-  //
-  // window.location.href = "https://accounts.spotify.com/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + URI + "&response_type=token&state=123";
+const searchForm = document.querySelector('.header__search-form');
+const searchInput = document.querySelector('.header__search-form input');
 
-
-  const searchForm = document.querySelector('.header__search-form');
-  const searchInput = document.querySelector('.header__search-form input');
-
-
-  let state = {
-    formInput: ''
-  };
-
-  function onSearchFormSubmit(e) {
-    e.preventDefault();
-    console.dir(searchInput.value);
-
-   fetch('https://accounts.spotify.com/authorize')
-     .then(data => console.log(data));
-
-    // fetch('https://api.spotify.com/v1/search?q=' + searchInput.value + 'tede&type=artist', {
-    //   method: 'GET',
-    //   headers: new Headers({
-    //     Accept: "application/json",
-    //     Authorization: "Bearer BQBzGTlZhFA5czOFQ7s4rfHadJZ6QVzhtEKeGr4l6jOv9AQ6trtuZLz90CbFAJPtPM4HtoBptGJgqBom3YYReDH4cI4dHWR1D9CrSLcbCx12rRyaMVH4QOhjAXJBColNOEDl7u2sd_Oyrj8"
-    //   })
-    // })
-    //   .then(data => data.json())
-    //   .then(res => console.log(res));
-
+async function onSearchFormSubmit(e) {
+  e.preventDefault();
+  try {
+    const results = await getArtistResults(searchInput.value);
+  } catch(e) {
+    console.error(e);
   }
+}
 
-  searchForm.addEventListener('submit', onSearchFormSubmit);
+async function getArtistResults(artistName) {
+  const headers = new Headers({
+    'Authorization': `Bearer ${JSON.parse(localStorage.getItem('playlistSpotifyToken')).token}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  });
+  const SEARCH_ARTIST_URL = `${SEARCH_URL}${artistName}&type=artist`;
+  const response = await fetch(SEARCH_ARTIST_URL, {headers});
+  const artistData = await response.json();
 
-  let token = window.location.hash.split('&')
-    .filter(e => e.match('access_token') !== null)
-    [0].split('=')[1];
-  console.log(token);
+  if (artistData.error) {
+    throw new Error(artistData.error.message);
+  }
+  return artistData
+}
 
-    //
+function checkToken() {
+  let token;
+  const localStorageToken = JSON.parse(localStorage.getItem('playlistSpotifyToken'));
+  if (localStorageToken) return;
 
+  const tokenInURL =  window.location.hash.includes('access_token');
+  if (!tokenInURL) {
+    window.location.href = "https://accounts.spotify.com/authorize?client_id=" + CLIENT_ID + "&redirect_uri=" + URI + "&response_type=token&state=123";
+  } else {
+    token = window.location.hash.split('&')
+      .filter(el => el.match('access_token') !== null)[0]
+      .split('=')[1];
+    const tokenObj = {
+      token,
+      timeStamp: new Date().getTime()
+    };
+    localStorage.setItem('playlistSpotifyToken', JSON.stringify(tokenObj));
+  }
+}
 
+checkToken();
 
-
-
-});
+searchForm.addEventListener('submit', onSearchFormSubmit);
